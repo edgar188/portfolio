440a0c9ba490c59749ffdb3689e0cc4ceb91b774
edgar <edgar.hovhannisyan08@gmail.com>
2023-01-02 12:38:57 +0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import Home from "./components/Home"
+import About from "./components/About";
+import Contact from "./components/Contact";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+      </ul>
+      </div>
+
+      <Routes>
+        <Route exact path='/' element={< Home />}></Route>
+        <Route exact path='/about' element={< About />}></Route>
+        <Route exact path='/contact' element={< Contact />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
