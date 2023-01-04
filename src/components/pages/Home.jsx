72ad7350a1969322087b9a1038147998484853c8
edgar <edgar.hovhannisyan08@gmail.com>
2023-01-04 23:30:24 +0400

    Added animations to the project

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { lightSpeedIn } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+
+const styles = {
+  lightSpeedIn: {
+    animation: 'x 2s',
+    animationName: Radium.keyframes(lightSpeedIn, 'lightSpeedIn')
+  }
+}
 
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-      <Button color='primary' variant='outlined' size='medium'>Primary button</Button>
-    </div>
+    <StyleRoot>
+      <h2>Homeeeee</h2>
+      <div style={styles.lightSpeedIn}>
+        <Button color='primary' variant='outlined' size='medium'>Primary</Button>
+      </div>
+    </StyleRoot>    
   )
 }
