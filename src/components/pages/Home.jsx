0342f942529be850f33b3f1d0de95c1f62530729
Edgar <edgar.hovhannisyan08@gmail.com>
2023-01-04 16:10:43 +0400

    Added material

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <div>
       <h2>Home</h2>
+      <Button color='primary' variant='outlined' size='medium'>Primary button</Button>
     </div>
   )
 }
