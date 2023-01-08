fcc0a8529bca649baa389c68b0691930c8b93bd4
edgar <edgar.hovhannisyan08@gmail.com>
2023-01-08 19:06:41 +0400

    About page start

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,46 @@ import React from 'react';
 
 export default function About() {
   return (
-    <section>
+    <section className='about'>
+      <div className='container'>
+        <div>
+          <h3>CORE SKILLS</h3>
+          <ul>
+            <li>Ruby, Ruby on Rails, Redis, RSpec, Sidekiq</li>
+            <li>MySQL, PostgreSQL</li>
+            <li>JavaScript(ES6), React, Redux, Preact, jQuery, CoffeeScript</li>
+            <li>HTML5, CSS3, SASS(SCSS), LESS, Bootstrap, Material-UI</li>
+            <li>Figma, Adobe Photoshop, Adobe XD</li>
+          </ul>
+          <h3>PROFESSIONAL EXPERIENCE</h3>
+          <ul>
+            <li>RUBY DEVELOPER</li>
+            <li>FRONT-END DEVELOPER</li>
+          </ul>
+          <h3>EDUCATION</h3>
+          <ul>
+            <li></li>
+          </ul>
+          <h3>COURSES</h3>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h3>ADDITIONAL SKILLS</h3>
+          <ul>
+            <li></li>
+          </ul>
+          <h3>LANGUAGES</h3>
+          <ul>
+            <li></li>
+          </ul>
+          <h3>INTERESTS</h3>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
