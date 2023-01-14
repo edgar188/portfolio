e8ea998a600b86d862da67259c9ea82588458e62
edgar <edgar.hovhannisyan08@gmail.com>
2023-01-15 00:37:13 +0400

    Content updates

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,17 +4,18 @@ export default function About() {
   return (
     <section className='about pt-5 pb-5'>
       <div className='container dsp-flex'>
-        <div>
+        <div className='section-left'>
           <h3>CORE SKILLS</h3>
-          <ul className='list-about mt-3 mb-4'>
+          <ul className='list-about'>
             <li>Ruby, Ruby on Rails, Redis, RSpec, Sidekiq</li>
             <li>MySQL, PostgreSQL</li>
             <li>JavaScript(ES6), React, Redux, Preact, jQuery, CoffeeScript</li>
             <li>HTML5, CSS3, SASS(SCSS), LESS, Bootstrap, Material-UI</li>
             <li>Figma, Adobe Photoshop, Adobe XD</li>
+            <li>Jira, Trello, Linear</li>
           </ul>
           <h3>PROFESSIONAL EXPERIENCE</h3>
-          <ul className='list-about mt-3 mb-4'>
+          <ul className='list-about'>
             <li>
               <p className='mb-1'>RUBY DEVELOPER</p>
               <p>March 2020 - Present | STDev Armenia | Yerevan, Armenia</p>
@@ -27,50 +28,60 @@ export default function About() {
                 <li>Was a mentor of the Ruby/RoR internship program.</li>
               </ul>
             </li>
-            <li>
+            <li className='mt-3'>
               <p className='mb-1'>FRONT-END DEVELOPER</p>
               <p>September 2019 - February 2020 | STDev Armenia | Yerevan, Armenia</p>
+              <ul className='mt-2'>
+                <li>Decided on the design and structure of the web page.</li>
+                <li>Developed features to enhance the users experience.</li>
+                <li>Made sure web pages are optimized for the best speed and scalability.</li>
+              </ul>
+            </li>
+            <li className='mt-3'>
+              <p className='mb-1'>FRONT-END DEVELOPER</p>
+              <p>June 2018 - September 2019 | Collaboratous | Yerevan, Armenia</p>
               <ul className='mt-2'>
                 <li>Worked on creating websites with fully responsive user interface design.</li>
                 <li>Participates in the development of templates in the style of Perfect Pixel.</li>
                 <li>Skilled in cross-browser/cross-platform issues and web standards.</li>
-                <li>For lack of time, made some parts of the site myself without a preliminary design template, trying to keep the best user interface practices and experience from previous designs.</li>
               </ul>
             </li>
           </ul>
-
         </div>
 
-        <div>
+        <div className='section-right'>
           <h3>EDUCATION</h3>
-            <ul className='list-about mt-3 mb-4'>
+            <ul className='list-about'>
               <li>
                 <p className='mb-1'>NATIONAL POLYTECHNIC UNIVERSITY OF ARMENIA</p>
-                M.S. in Engineering: Design and Modelling of Mechanical Systems | 2020-2022 | Yerevan, Armenia
+                <span>M.S. in Engineering: Design and Modelling of Mechanical Systems | 2020-2022 | Yerevan, Armenia</span>
               </li>
-              <li>
-                <p className='mb-1 mt-2'>NATIONAL POLYTECHNIC UNIVERSITY OF ARMENIA</p>
-                B.S. in Engineering: Computer Design of Mechanical Systems | 2016-2020 | Yerevan, Armenia
+              <li className='mt-3'>
+                <p className='mb-1'>NATIONAL POLYTECHNIC UNIVERSITY OF ARMENIA</p>
+                <span>B.S. in Engineering: Computer Design of Mechanical Systems | 2016-2020 | Yerevan, Armenia</span>
               </li>
             </ul>
             <h3>COURSES</h3>
-            <ul className='list-about mt-3 mb-4'>
-              <li>ARMENIAN CODE ACADEMY JavaScript/React Development | 2019 | Yerevan, Armenia</li>
+            <ul className='list-about'>
+              <li>
+                <p className='mb-1'>ARMENIAN CODE ACADEMY</p>
+                <span>JavaScript/React Development | 2019 | Yerevan, Armenia</span>
+              </li>
             </ul>
           <h3>ADDITIONAL SKILLS</h3>
-          <ul className='list-about mt-3 mb-4'>
+          <ul className='list-about'>
             <li>Work overtime based on team success and project outcomes.</li>
             <li>Good team player and willingness to help others.</li>
             <li>Mentoring.</li>
           </ul>
           <h3>LANGUAGES</h3>
-          <ul className='list-about mt-3 mb-4'>
+          <ul className='list-about'>
             <li>Armenian(Native)</li>
             <li>English(Good)</li>
             <li>Russian(Good)</li>
           </ul>
           <h3>INTERESTS</h3>
-          <ul className='list-about mt-3 mb-4'>
+          <ul className='list-about'>
             <li>Classical guitar playing, gaming, hiking, and swimming.</li>
           </ul>
         </div>
